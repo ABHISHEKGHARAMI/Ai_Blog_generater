1adfa0c9309287e2c745745ba0923368325b8689
abhishek <abhishekgharami1998@gmail.com>
2025-02-02 19:37:52 +0530

    Added the views.py for the urls.py.

--- ai_blog_app/blog_generator/urls.py
+++ ai_blog_app/blog_generator/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('login/',views.login,name='index'),
-    path('signup/',views.signup,name='index'),
-    path('logout/', views.logout, name='index'),
+    path('login/',views.user_login,name='login'),
+    path('signup/',views.user_signup,name='signup'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- ai_blog_app/blog_generator/views.py
+++ ai_blog_app/blog_generator/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 def index(request):
     return render(request,
                   'index.html')
+
+
+# login view
+def user_login(request):
+    return render(request,
+                  'login.html')
+
+# sign up page
+def user_signup(request):
+    return render(request,
+                  'signup.html')
+
+# logout view
+def user_logout(request):
+    pass
+
