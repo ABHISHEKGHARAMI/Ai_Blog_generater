80bc31a4d81261d7bae962ac58f198a5251fc1d7
abhishek <abhishekgharami1998@gmail.com>
2025-02-03 12:11:39 +0530

    login working.

--- ai_blog_app/blog_generator/views.py
+++ ai_blog_app/blog_generator/views.py
@@ -13,7 +13,7 @@ def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(request,username,password)
+        user = authenticate(request,username=username,password=password)
         if user is not None:
             login(request,user)
             return redirect('/')
