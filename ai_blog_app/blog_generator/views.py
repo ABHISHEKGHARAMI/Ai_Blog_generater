797f609684b197b52445c503b8c2ed5c9507b89c
abhishek <abhishekgharami1998@gmail.com>
2025-02-03 11:57:42 +0530

    checking out the login view for the user.

--- ai_blog_app/blog_generator/views.py
+++ ai_blog_app/blog_generator/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate,logout
 
 # Create your views here.
 def index(request):
@@ -8,11 +10,48 @@ def index(request):
 
 # login view
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request,username,password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+        else:
+            error_message = 'unfortunately this user does not exist...'
+            return render(request,'login.html',
+                          {
+                              'error_message':error_message
+                          })
     return render(request,
                   'login.html')
 
 # sign up page
 def user_signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        repeatPassword = request.POST.get('repeatPassword')
+        if password == repeatPassword:
+            try:
+                user = User.objects.create_user(username,email,password)
+                user.save()
+                login(request,user)
+                return redirect('/')
+            except Exception:
+                error_message = 'error creating the user profile..'
+                return render(request,
+                              'signup.html',
+                              {
+                                  'error_message':error_message
+                              })
+        else:
+            error_message = 'password does not match'
+            return render(request,
+                          'signup.html',{
+                              'error_message':error_message
+                          })
     return render(request,
                   'signup.html')
 
