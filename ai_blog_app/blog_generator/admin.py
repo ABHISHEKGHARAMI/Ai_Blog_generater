c0339688e95e06c8e5c6722ae235e7c04b930510
abhishek <abhishekgharami1998@gmail.com>
2025-02-05 12:12:00 +0530

    Registered the model .

--- ai_blog_app/blog_generator/admin.py
+++ ai_blog_app/blog_generator/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import BlogPost
 # Register your models here.
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ['user','youtube_title','created_at']
+    list_filter = ['created_at']
+    search_fields = ['created_at','youtube_title']
